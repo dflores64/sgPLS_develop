86466484ba01aa7fb5bc562a6e9301c92aa0ce73
Daniel-stage <dflor64fr@gmail.com>
2025-06-10 17:24:10 +0200

    Add files via upload

---
+++ Installation_depuis_github.R
@@ -0,0 +1,5 @@
+# installation depuis GitHub
+library(devtools)
+devtools::install_github('Daniel-stage/regression_PLS/sgPLS/R')
+devtools::install_github('Daniel-stage/regression_PLS/sgPLS')
+devtools::install_github('Daniel-stage/regression_PLS')
