0bafb6b5bb5d5762091d7747c9efe293007afc7e
Flores <dflor64fr@gmail.com>
2025-06-13 16:40:39 +0200

    Update create.data.R

--- R/create.data.R
+++ R/create.data.R
@@ -1,8 +1,8 @@
 create.data <- function(n = 40, p = 10, q = 1){
   X <- matrix(data = runif(n*p),n,p)
   U <- matrix(data = runif(q*p,-10,10), nrow = p, ncol = q)
-  E <- matrix(data = rnorm(n*p),n,q)
+  E <- matrix(data = rnorm(n*q),n,q)
   Y <- X%*%U + E
   D <- data.frame(X,Y)
-  return(list(X = X,Y = Y))
+  return(list(D = D, X = X, Y = Y))
 }
