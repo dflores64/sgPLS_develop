3d2ecf27e438912dbf2a8ab924b8fcdcda16c26b
Flores <dflor64fr@gmail.com>
2025-06-30 15:26:10 +0200

    Update ZPLS-PLSda.R

--- R/ZPLS-PLSda.R
+++ R/ZPLS-PLSda.R
@@ -76,3 +76,36 @@ PLS <- function(X,Y,ncomp,mode){
   return(invisible(result))
 
 }
+
+# PLSda function ------------------------------------------
+PLSda <-
+  function(X, 
+           Y,		
+           ncomp = 2, 
+           keepX = rep(ncol(X), ncomp),
+           max.iter = 500,		 
+           tol = 1e-06)
+  {
+    
+    # Testing the input Y
+    if (is.null(dim(Y)))
+    {
+      Y = as.factor(Y)	
+      ind.mat = unmap(as.numeric(Y))					
+    }else {
+      stop("'Y' should be a factor or a class vector.")						
+    }		
+    
+    result = PLS(X, ind.mat, ncomp = ncomp, mode = "regression")
+    
+    cl = match.call()
+    cl[[1]] = as.name('sPLSda')
+    result$call = cl
+    
+    result$ind.mat = ind.mat
+    result$names$Y = levels(Y)
+    
+    class(result) = c("sPLSda","splsda","plsda")
+    return(invisible(result))	
+  }
+
