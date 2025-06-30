066224f3320528e9afbba6a3a7f6cbde00bddeab
Flores <dflor64fr@gmail.com>
2025-06-30 14:58:11 +0200

    Delete R/create.data.R

--- R/create.data.R
+++
@@ -1,8 +0,0 @@
-create.data <- function(n = 40, p = 10, q = 1){
-  X <- matrix(data = runif(n*p),n,p)
-  U <- matrix(data = runif(q*p,-10,10), nrow = p, ncol = q)
-  E <- matrix(data = rnorm(n*q),n,q)
-  Y <- X%*%U + E
-  D <- data.frame(X,Y)
-  return(list(D = D, X = X, Y = Y))
-}
