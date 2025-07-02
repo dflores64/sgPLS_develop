ffbfe39b255cb00759ea60e48360dabf3d65931c
Flores <dflor64fr@gmail.com>
2025-07-02 14:59:33 +0200

    perf2.PLSda added

--- R/perf.PLSda.R
+++ R/perf.PLSda.R
@@ -37,3 +37,25 @@ perf.PLSda <- function(object, ncomp = object$ncomp, method = "max.dist"){
   return(setNames(list(err.moy,h.best),c("error","h.best")))
 }
 
+perf2.PLSda <- function(object, ncomp = object$ncomp, method = "max.dist"){
+  
+  X <- object$X
+  Y <- map(object$Y)
+  n <- nrow(X)
+  p <- ncol(X)
+  
+  if(method == "max.dist"){k=1}else if(method == "centroids.dist"){k=2}else{k=3}
+  
+  # prediction analysis
+  err.moy <- numeric(ncomp)
+  matconf <- list()
+  
+  for(h in 1:ncomp){
+    err.moy[h] <- CV(X,Y,mode = "class",ncomp = h,K=n)
+  }
+  
+  h.best <- min(which.min(err.moy))
+  plot(err.moy, col="blue", pch = 16, type = "b", main = "Error rate of the model", xlab = "number of components", ylab = "Error")
+  
+  return(setNames(list(err.moy,h.best),c("error","h.best")))
+}
