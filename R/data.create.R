702e24ad253609f458d7193155cb8a53db024e8d
Flores <dflor64fr@gmail.com>
2025-07-30 10:14:01 +0200

    Update data.create.R

--- R/data.create.R
+++ R/data.create.R
@@ -1,21 +1,25 @@
-data.create <- function(n = 40, p = 10, q = 1){
-  X <- matrix(data = runif(n*p),n,p)
-  U <- matrix(data = runif(q*p,-10,10), nrow = p, ncol = q)
-  E <- matrix(data = rnorm(n*q),n,q)
-  Y <- X%*%U + E
+data.create <- function(n = 40, p = 3, q = 1, list = FALSE){  
+  X <- matrix(data = rnorm(n*p),n,p)
+  B <- matrix(data = runif(q*p,-1,1), nrow = p, ncol = q)
+  E <- matrix(data = rnorm(n*q, sd = 0.1),n,q)
+  Y <- X%*%B + E
+  
+  if(p > 1){colnames(X) <- paste0(rep("X",p),1:p)}
+  if(q > 1){colnames(Y) <- paste0(rep("Y",q),1:q)}
   D <- data.frame(X,Y)
-  colnames(X) <- paste0(rep("X",p),1:p)
-  return(list(D = D,X = X,Y = Y))
+  
+  if(list){return(list(D = D,X = X,Y = Y))}else{return(D)}
 }
 
-data.cl.create <- function(n = 40, p = 10, classes = 2){
+data.cl.create <- function(n = 40, p = 3, classes = 2, list = FALSE){
   X <- matrix(nrow = n, ncol = p)
   Y <- numeric(n)
   for(i in seq_len(n)){
     Y[i] <- sample(x = seq_len(classes), size = 1)
-    X[i,] <- runif(p) + Y[i] + rnorm(p)
+    X[i,] <- rnorm(p) + Y[i] + rnorm(p, sd = 0.1)
   }
+  
+  if(p > 1){colnames(X) <- paste0(rep("X",p),1:p)}
   D <- data.frame(X,Y)
-  colnames(X) <- paste0(rep("X",p),1:p)
-  return(list(D = D,X = X,Y = Y))
+  if(list){return(list(D = D,X = X,Y = Y))}else{return(D)}
 }
