fbfb167b4b8dd9fcc6773f4100daae12ef4112b9
Flores <dflor64fr@gmail.com>
2025-07-31 10:11:55 +0200

    Update data.create.R

--- R/data.create.R
+++ R/data.create.R
@@ -1,7 +1,7 @@
 data.create <- function(n = 40, p = 3, q = 1, list = FALSE){  
   X <- matrix(data = rnorm(n*p),n,p)
   B <- matrix(data = runif(q*p,-1,1), nrow = p, ncol = q)
-  E <- matrix(data = rnorm(n*q, sd = 0.1),n,q)
+  E <- matrix(data = rnorm(n*q, sd = 0.05),n,q)
   Y <- X%*%B + E
   
   if(p > 1){colnames(X) <- paste0(rep("X",p),1:p)}
@@ -16,7 +16,7 @@ data.cl.create <- function(n = 40, p = 3, classes = 2, list = FALSE){
   Y <- numeric(n)
   for(i in seq_len(n)){
     Y[i] <- sample(x = seq_len(classes), size = 1)
-    X[i,] <- rnorm(p) + Y[i] + rnorm(p, sd = 0.1)
+    X[i,] <- rnorm(p) + Y[i] + rnorm(p, sd = 0.05)
   }
   
   if(p > 1){colnames(X) <- paste0(rep("X",p),1:p)}
