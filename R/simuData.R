ba573454d6927278110e12f9ee8fc333d5161856
Flores <dflor64fr@gmail.com>
2025-09-18 17:14:27 +0200

    Add files via upload

---
+++ R/simuData.R
@@ -0,0 +1,10 @@
+#' Example simulated dataset
+#'
+#' A toy dataset for testing sgPLSdevelop functions.
+#'
+#' @format A data frame with 48 rows and 1000 X variables:
+#' \describe{
+#'   \item{X}{double}
+#'   \item{Y}{factor}
+#' }
+"simuData"
